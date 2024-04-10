71b9bc6edbf8536292fa74c0954d43600eed001c
Priyesh04 <zopepriyesh2001@gmail.com>
2024-04-10 14:10:34 +0530

    second commit

--- obj/Debug/net8.0/HelloWorldWeb.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorldWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3338cdf913026473d800ad8a9a8cb3684ff06a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorldWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
